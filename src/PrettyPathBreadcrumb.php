be5b2cc76451cde394766357ef09aec7a296bfcd
StryKaizer <StryKaizer@462700.no-reply.drupal.org>
2018-03-22 15:59:22 +0100

    Issue #2955263 by StryKaizer: Breadcrumb support

---
+++ src/PrettyPathBreadcrumb.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\facets_pretty_paths;
+
+use Drupal\Core\Breadcrumb\Breadcrumb;
+use Drupal\Core\Breadcrumb\BreadcrumbBuilderInterface;
+use Drupal\Core\Link;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\Url;
+use Drupal\Core\Routing\RouteMatchInterface;
+
+class PrettyPathBreadcrumb implements BreadcrumbBuilderInterface {
+  use StringTranslationTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function applies(RouteMatchInterface $route_match) {
+    if ($route_match->getParameter('facets_query')) {
+      return TRUE;
+    }
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(RouteMatchInterface $route_match) {
+    $breadcrumb = new Breadcrumb();
+    $breadcrumb->addCacheContexts(['route']);
+
+    $route_object = $route_match->getRouteObject();
+    $route_without_facets_query = explode('/{facets_query}', $route_object->getPath())[0];
+
+    $request = \Drupal::request();
+    $title = \Drupal::service('title_resolver')
+      ->getTitle($request, $route_object);
+
+    $url = Url::fromUserInput($route_without_facets_query);
+    $links[] = Link::createFromRoute($this->t('Home'), '<front>');
+    $links[] = Link::fromTextAndUrl($title, $url);
+    return $breadcrumb->setLinks($links);
+  }
+}
