3a2ab9584e85c08be74f1795f1d5489144d1e224
upchuk <danny@webomelette.com>
2018-11-14 19:07:14 +0100

    GH-1 - Dockerise and enable travis.

---
+++ tests/src/Kernel/ExampleKernelTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Drupal\Tests\facets_pretty_paths\Kernel;
+
+use Drupal\Tests\token\Kernel\KernelTestBase;
+
+/**
+ * Example Kernel test.
+ */
+class ExampleKernelTest extends KernelTestBase {
+
+  /**
+   * Example test.
+   */
+  public function testExample() {
+    $this->assertEquals(1, 1);
+  }
+}
